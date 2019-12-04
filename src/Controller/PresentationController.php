8dd1752f223567e68eca22e85c0b5187a1319819
Mikaël MAROLLEAU-POIRIER <mikael.mp@icloud.com>
2019-12-04 14:05:39 +0100

    correction erreur travis presentationController n3

--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -13,9 +13,9 @@ class PresentationController extends AbstractController
     /**
     * @Route("/presentation", name="app_presentation")
     */
-    public function index(PresentationRepository $presentationRepository) :Response
+    public function index(PresentationRepository $presRepository) :Response
     {
-        $presentations = $presentationRepository->findAll();
+        $presentations = $presRepository->findAll();
 
         if (!$presentations) {
             throw $this->createNotFoundException('Aucune information n\'est disponible.');
