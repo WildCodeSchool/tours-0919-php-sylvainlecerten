44018a6b4eb95357e488f4c8b3118e578ad32a09
Mikaël MAROLLEAU-POIRIER <mikael.mp@icloud.com>
2019-12-05 11:48:51 +0100

    saving form presentation add wip

--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -5,8 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use App\Entity\Presentation;
 use App\Repository\PresentationRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\PresType;
+
 
 class PresentationController extends AbstractController
 {
@@ -23,5 +30,29 @@ class PresentationController extends AbstractController
         return $this->render('presentation.html.twig', [
                 'presentations' => $presentations
         ]);
+    }
+
+    /**
+    * @Route("/admin", name="app_administration")
+    */    
+    public function addParagraph(Request $request)
+    {
+        $presentation = new Presentation();
+        $form = $this->createForm(PresType::class, $presentation);
+        $form->handleRequest($request);
+     
+        if ($form->isSubmitted() && $form->isValid()) {
+         $emm = $this->getDoctrine()->getManager();
+         $emm->persist($presentation);
+         $emm->flush();
+         
+         return $this->redirectToRoute('/presentation');
+        }
+     
+        return $this->render('form.html.twig', [
+         'paragraph' => $presentation,
+         'form' => $form->createView(),
+        ]);
+     
     }
 }
---
+++ src/Form/PresType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Presentation;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class PresType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('subtitle', TextType::class)
+            ->add('paragraph', TextType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Presentation::class,
+        ]);
+    }
+}
