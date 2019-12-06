55b3ba8e9a516c8029044e5167c7fcf1e2009b89
Mikaël MAROLLEAU-POIRIER <mikael.mp@icloud.com>
2019-12-06 17:15:22 +0100

    formulaire presentation fonctionnel

--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Presentation;
 use App\Repository\PresentationRepository;
+use App\Form\PresentationType;
 
 class PresentationController extends AbstractController
 {
@@ -23,5 +25,27 @@ class PresentationController extends AbstractController
         return $this->render('presentation.html.twig', [
                 'presentations' => $presentations
         ]);
+    }
+    /**
+    * @Route("/new", name="presentation_new")
+    */
+    public function newPresentation(Request $request): Response
+    {
+    $presentation = new Presentation();
+    $presentationForm = $this->createForm(PresentationType::class, $presentation);
+    $presentationForm->handleRequest($request);
+
+    if ($presentationForm->isSubmitted() && $presentationForm->isValid()) {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($presentation);
+        $em->flush();
+        
+        return $this->redirectToRoute('app_presentation');
+    }
+
+    return $this->render('form.html.twig', [
+        'presentation' => $presentation,
+        'form' => $presentationForm->createView(),
+    ]);
     }
 }
