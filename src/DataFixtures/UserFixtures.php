fa338001741d1525d2d646e8dad9e32162501051
Mikaël MAROLLEAU-POIRIER <mikael.mp@icloud.com>
2020-01-03 16:07:12 +0100

    admin en cours - pb technique hashage mdp

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('coucou');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'coucou'
+        ));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
