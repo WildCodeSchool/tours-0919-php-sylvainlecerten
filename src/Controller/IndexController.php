331fb3fbe7e1451d6cb3c14b6e779600c556f6a2
Mikaël MAROLLEAU-POIRIER <mikael.mp@icloud.com>
2020-01-06 17:23:00 +0100

    entity message + MessageRepo + MessageType+ index + twig

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Content;
 use App\Repository\ContentRepository;
+use App\Repository\MessageRepository;
+use App\Form\MessageType;
 
 class IndexController extends AbstractController
 {
@@ -25,9 +27,16 @@ class IndexController extends AbstractController
             ['ordering' => 'ASC']
         );
 
+        $form = $this->createForm(
+            MessageType::class,
+            null,
+            ['method' => Request::METHOD_GET]
+        );
+
         return $this->render('index/index.html.twig', [
             'whoAmIContents' => $whoAmIContents,
-            'presentationContents' => $presentationContents
+            'presentationContents' => $presentationContents,
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/MessageType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $options;
+        $builder
+            ->add('Prenom')
+            ->add('Nom')
+            ->add('Societe')
+            ->add('Message')
+            ->add('Telephone')
+            ->add('Email')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+        ]);
+    }
+}
