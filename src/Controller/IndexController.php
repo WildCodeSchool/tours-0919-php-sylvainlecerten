c4298a8f5071fe92ea5a96c1e68a9bd48539dccd
Mayte <malapert.mayte@gmail.com>
2020-01-08 11:23:25 +0100

    affichage services+fixtures

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Content;
+use App\Entity\Service;
+use App\Repository\ServiceRepository;
 use App\Repository\ContentRepository;
 
 class IndexController extends AbstractController
@@ -14,7 +16,7 @@ class IndexController extends AbstractController
     /**
     * @Route("/", name="app_index")
     */
-    public function index(ContentRepository $contentRepository) :Response
+    public function index(ContentRepository $contentRepository, ServiceRepository $serviceRepository) :Response
     {
         $whoAmIContents = $contentRepository->findBy(
             ['category' => 'whoami'],
@@ -25,9 +27,13 @@ class IndexController extends AbstractController
             ['ordering' => 'ASC']
         );
 
+        $services = $serviceRepository->findAll();
+
+        dump($services);
         return $this->render('index/index.html.twig', [
             'whoAmIContents' => $whoAmIContents,
-            'presentationContents' => $presentationContents
+            'presentationContents' => $presentationContents,
+            'services' => $services
         ]);
     }
 }
