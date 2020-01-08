8f38440eff9ad81f2a3331441e06ea7fcb8191a9
Lirith-Drasar <ma.rabouin@laposte.net>
2020-01-08 16:54:39 +0100

    Remise à version Migration

--- src/Migrations/Version20200108155404.php
+++ src/Migrations/Version20200108155404.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200108153740 extends AbstractMigration
+final class Version20200108155404 extends AbstractMigration
 {
     public function getDescription() : string
     {
