3f95ea3b9b9dbaec033c7772bb303cdd92a9eb56
Lirith-Drasar <ma.rabouin@laposte.net>
2020-01-16 15:21:12 +0100

    changement traduction connection changé avec deconnection et page de redirection fixer lors de la connection

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use \Exception;
 
 class SecurityController extends AbstractController
 {
@@ -31,5 +32,6 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
