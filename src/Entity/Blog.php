69e48bdbe63399c6a4913ff7979ad57e901c0da0
Mayte <malapert.mayte@gmail.com>
2020-02-03 12:09:34 +0100

    Entity blog created whith is crud en ficture need more secure

---
+++ src/Entity/Blog.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BlogRepository")
+ */
+class Blog
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $subtitles;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $publication;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSubtitles(): ?string
+    {
+        return $this->subtitles;
+    }
+
+    public function setSubtitles(string $subtitles): self
+    {
+        $this->subtitles = $subtitles;
+
+        return $this;
+    }
+
+    public function getPublication(): ?string
+    {
+        return $this->publication;
+    }
+
+    public function setPublication(string $publication): self
+    {
+        $this->publication = $publication;
+
+        return $this;
+    }
+}
